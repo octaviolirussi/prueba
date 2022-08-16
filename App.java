ca8966fc2bd0fdc881cc5976d6b8747ac9676093
Octavio <octaviolirussi@gmail.com>
2022-08-16 16:34:59 -0300

    nuevo

--- App.java
+++ App.java
@@ -2,5 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("buenas");
+        int x=2;
+        x+=2;
+        System.out.println(x);
     }
 }
